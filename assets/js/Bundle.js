0da1cab739cf5fd1a786d6ada9c4e21b69f0ce6d
Albert Saitov <albert.saitov@ylab.io>
2020-04-21 12:24:19 +0500

    21 April commit

--- assets/js/Bundle.js
+++ assets/js/Bundle.js
@@ -39,4 +39,27 @@ $(document).ready( function() {
             }
         })
     })
+
+    /**
+     * Кастомный Input File
+     */
+    let inputs = document.querySelectorAll( '.input__file__target' )
+
+    Array.prototype.forEach.call(inputs, function(input) {
+        let label	 = input.nextElementSibling,
+            labelVal = label.innerHTML
+
+        input.addEventListener('change', function(e) {
+            let fileName = ''
+
+            if (this.files && this.files.length > 1) {
+                fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length )
+            } else {
+                fileName = e.target.value.split('\\').pop()
+            }
+
+            if (fileName) label.innerHTML = fileName
+            else label.innerHTML = labelVal
+        })
+    })
 })
